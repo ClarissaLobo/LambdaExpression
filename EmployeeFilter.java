889cf4e5d3ee49d1b91d9d0cda51b4f9a1cb2f76
Clarissa Lobo <139502710+ClarissaLobo@users.noreply.github.com>
2025-04-10 00:26:14 -0400

    EmployeeFilter predicate/Consumer

--- EmployeeFilter.java
+++ EmployeeFilter.java
@@ -1,6 +1,9 @@
-//Filter employees based on salary/department and display their info
+//Filter employees based on hike,salary/department and display their info
 
 import java.util.ArrayList;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 class Employee{
     String name;
@@ -23,7 +26,23 @@ public class EmployeeFilter {
         emplist.add(new Employee("Sophie", 30000,"ES"));
         emplist.add(new Employee("Rani", 25000,"ME"));
 
+        //Predicate
+        Predicate<Integer> p=b->b>=30000;
+
+        Consumer<Employee> c=emp->{
+            System.out.println(emp.name);
+            System.out.println(emp.salary);
+            System.out.println(emp.department);
+        	};
+        for(Employee e:emplist){
+        	if(p.test(e.salary)){      //Invoked Predicate
+        		c.accept(e);        // Invoked consumer
+        		System.out.println(e.name);
+        	}
+        }
+
         
+
 
     }
     
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # LambdaExpression
+
+LambdaExpression
+-> It is an Anonymous funtion
+-> No method name
+-> No Return Type
+-> No access modifier
+
+Functional Interface
+-> Runnable - run()
+-> Callable - call()
+-> Comparable - compareTo()
+-> ActionListener - actionPerformed()
+
+Pre-defined Functional Interface
+- Predicate
+- Function
+- Consumer
+- Supplier
